96a2b1663e756ba74f732dcfa3cdf6a5399f7c2e
matuneville <nevillematias@gmail.com>
2023-04-17 10:58:20 -0300

    labo04

---
+++ Laboratorios/Labo04/cmake-build-debug/CMakeFiles/ej1.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/neville/GitHub/uba-algo2/Laboratorios/Labo04/src/ConexionJugador.cpp" "CMakeFiles/ej1.dir/src/ConexionJugador.cpp.o" "gcc" "CMakeFiles/ej1.dir/src/ConexionJugador.cpp.o.d"
+  "/home/neville/GitHub/uba-algo2/Laboratorios/Labo04/src/Internet.cpp" "CMakeFiles/ej1.dir/src/Internet.cpp.o" "gcc" "CMakeFiles/ej1.dir/src/Internet.cpp.o.d"
+  "/home/neville/GitHub/uba-algo2/Laboratorios/Labo04/src/SistemaDeMensajes.cpp" "CMakeFiles/ej1.dir/src/SistemaDeMensajes.cpp.o" "gcc" "CMakeFiles/ej1.dir/src/SistemaDeMensajes.cpp.o.d"
+  "/home/neville/GitHub/uba-algo2/Laboratorios/Labo04/tests/sdm_tests.cpp" "CMakeFiles/ej1.dir/tests/sdm_tests.cpp.o" "gcc" "CMakeFiles/ej1.dir/tests/sdm_tests.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/neville/GitHub/uba-algo2/Laboratorios/Labo04/cmake-build-debug/CMakeFiles/gtest.dir/DependInfo.cmake"
+  "/home/neville/GitHub/uba-algo2/Laboratorios/Labo04/cmake-build-debug/CMakeFiles/gtest_main.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
